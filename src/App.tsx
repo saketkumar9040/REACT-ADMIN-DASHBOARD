0e396b51f553f92afc03fd0638d8e34772a205a3
SAKET KUMAR <106534885+saketkumar9040@users.noreply.github.com>
2024-01-24 23:22:51 +0530

    style - added scss properties in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
-import { lazy } from "react";
+import { Suspense, lazy } from "react";
+import Loader from "./components/loader";
 
 const Dashboard = lazy(()=>import("./pages/dashboard"))
+const Customers = lazy(()=>import("./pages/customer"))
+const Products = lazy(()=>import("./pages/products"))
+const Transaction = lazy(()=>import("./pages/transaction"))
 
 const App = () => {
   return (
     <Router>
-      <Routes>
+    <Suspense fallback={<Loader/>}>
+    <Routes>
         <Route path="/admin/dashboard" element={<Dashboard/>}/>
+        <Route path="/admin/customers" element={<Customers/>}/>
+        <Route path="/admin/products" element={<Products/>}/>
+        <Route path="/admin/transaction" element={<Transaction/>}/>
       </Routes>
+    </Suspense>
     </Router>
   )
 }
